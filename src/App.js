146638524e857aa1df524f0af20f33e93afbefac
Shaliko <h.shaliko@mail.ru>
2021-04-28 17:46:21 +0400

    aa

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Test';
+import SignIn from './SignIn';
+
+
+function App() {
+  return (
+      <SignIn />
+  );
+}
+
+export default App;
